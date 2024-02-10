301561c5cd08e9aa11c706fc5f00c3f92e9d4423
diego <diegozaratean@gmail.com>
2024-02-10 02:30:29 +0000

    finish js intro class

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('desde archivo');
+
+let nombre = 'diegp';
+
+console.log(nombre);
+
+
+//. posición   0.          1.         2
+let frutas = ['manzana', 'banano', 'uva']
+// elemento.     1.          2.      3
+// longitud 3
