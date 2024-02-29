f708cd22dca0ff0962994d3258d902eb7a4e4400
diego <diegozaratean@gmail.com>
2024-02-29 02:18:52 +0000

    finish random card generator class 1

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let persona = {
     edad: 30,
     nacionalidad: 'mexicana',
     skills: ['js','python','react'],
+    'lugar de nacimiento': 'guadalajara jalisco',
     mascota:{
         nombre:'spanky',
         raza:'pastor ingles'
@@ -32,8 +33,11 @@ const perro = {
 
 function cambiarTitulo(){
     console.log('ejecutar algo ')
-    document.querySelector('#title').innerHTML = 'titulo azul'
-    document.querySelector('#title').style.backgroundColor = 'blue'
+    const names = ['Mario','Luigi','Toad','Peach','Bowser','star']
+    const colors = ['red','blue','green','violet','black']
+
+    document.querySelector('#title').innerHTML = 'Hola ' + names[Math.floor(Math.random() * names.length)]
+    document.querySelector('#title').style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
 }
 
 
@@ -45,3 +49,23 @@ function cambiarTitulo(){
 // 4. cambiar texto -DONE
 // 5. Integrar todo
 
+
+
+// PSEUDOCODIGO
+// Hagan un programa que lance dos dados
+// - DONE 1. cuando le de click a un boton ejecutar una accion
+// - DONE 2. generar dos numeros aletorio entre 1 y 6 - DONE
+// - DONE 3. graficas o mostrar de alguna forma que no sea en consola los dados al usuario
+// - DONE 4. asignar el numero aleatorio a los datos (desde js hasta html)
+// - DONE 4.1 asignar un valor desde js a html
+// - DONE 4.2 aaigna un numero aletorio de 1 a 6 a dado 1
+// - DONE 4.3 aaigna un numero aletorio de 1 a 6 a dado 2
+// 5. estilizar los dados para que se vean como dados
+
+console.log(Math.ceil(Math.random() * 6))
+
+function lanzarDados(){
+    console.log('lanzarDados')
+    document.getElementById('dado1').textContent = Math.ceil(Math.random() * 6)
+    document.querySelector('#dado2').innerText = Math.ceil(Math.random() * 6)
+}
